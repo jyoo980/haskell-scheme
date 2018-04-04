193f8ac165e6d3d61901cceadcd6d6d9419eaa37
James Yoo <jyoo980@gmail.com>
2018-04-03 21:56:33 -0700

    Began value binding

--- eval.hs
+++ eval.hs
@@ -1,16 +1,11 @@
 module Eval where
 import Data
 
-showVal :: LispVal -> String
-showVal (String contents) = "\"" ++ contents ++ "\'"
-showVal (Atom name) = name
-showVal (Number num) = show num
-showVal (Fractional frac) = show frac
-showVal (Bool True) = "#t"
-showVal (Bool False) = "#f"
-showVal (List contents) = "(" ++ unwordList contents ++ ")"
-showVal (DottedList head tail) =
-    "(" ++ unwordList head ++ "." ++ showVal tail ++ ")" 
-
-unwordList :: [LispVal] -> String
-unwordList = unwords . map showVal
+-- val@(String _) matches against any LispVal that's 
+-- a string and then binds val to the whole LispVal
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Fractional _) = val
+eval val@(Bool _) = val
+eval val@(List [Atom "quote", val]) = val
