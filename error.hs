3e205ff93eefd144365ce6197f01bee05b953eda
James Yoo <jyoo980@gmail.com>
2018-04-04 22:31:44 -0700

    Defined error.hs

--- error.hs
+++ error.hs
@@ -1,3 +1,11 @@
 module Error where
 import Control.Monad.Error
 
+data LispError = NumArgs Integer [LispVal]
+    | TypeMismatch String LispVal
+    | Parser ParseError
+    | BadSpecialForm String LispVal
+    | NotFunction String String
+    | UnboundVar String String
+    | Default String
+
